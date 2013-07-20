80c656924564a2e2041cbfd793ec274c2cc5b9f5
Conner Petzold <cpetzold@gmail.com>
2013-07-20 01:01:38 -0700

    playing with core.async, implement jsonp request fn and dom listen channel fn

--- project.clj
+++ project.clj
@@ -1,15 +1,17 @@
 (defproject prism "0.1.0-SNAPSHOT"
   :description "Prismatic frontend in cljs"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.async "0.1.0-SNAPSHOT"]
                  [noir "1.3.0"]
                  [garden "0.1.0-beta6"]
                  [prismatic/dommy "0.1.1"]]
-  :plugins [[lein-cljsbuild "0.2.9"]]
+  :plugins [[lein-cljsbuild "0.3.2"]]
   :source-paths ["src/clj"]
   :main prism.server
   :cljsbuild
   {:builds
-   [{:source-path "src/cljs"
+   [{:source-paths ["src/cljs"]
      :compiler
      {:output-to "resources/public/prism.js"
       :pretty-print true}}]})
--- src/cljs/prism/core.cljs
+++ src/cljs/prism/core.cljs
@@ -1,5 +1,36 @@
 (ns prism.core
+  (:require-macros
+   [cljs.core.async.macros :refer [go alt!]]
+   [dommy.macros :refer [node]])
   (:require
+   [cljs.core.async :refer [chan put! <! !>]]
    [dommy.core :as dommy]))
 
-(dommy/append! js/document.body [:h1 "Hello, world!"])
+(defn request* [url cb]
+  (let [fn-name (gensym "fn")
+        url (format "%s%scallback=%s"
+                    url
+                    (if (neg? (.indexOf url "?")) "?" "&")
+                    fn-name)
+        script (node [:script {:src url}])]
+    (aset js/window fn-name
+          (fn [res]
+            (dommy/remove! script)
+            (cb res)))
+    (dommy/append! js/document.head script)))
+
+(defn request [url]
+  (let [c (chan)]
+    (request* url #(put! c %))
+    c))
+
+(defn listen [el type]
+  (let [c (chan)]
+    (dommy/listen! el type #(put! c %))
+    c))
+
+(let [click (listen js/window :click)]
+  (go
+   (while true
+     (<! click)
+     (js/console.log (<! (request "//api.getprismatic.com/news/home"))))))
